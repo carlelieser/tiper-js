20ca7bd8b6cd17df7c52d62c125f80e32a292374
Carlos <carloseliesersantos@gmail.com>
2020-08-10 16:16:17 -0400

    Init test site

--- lib/utils.d.ts
+++
@@ -1,6 +0,0 @@
-declare const Utils: {
-    getRandomArbitrary: (min: number, max: number) => number;
-    shuffleArray: (arr: any) => any[];
-    trim: (str: string) => string;
-};
-export = Utils;
